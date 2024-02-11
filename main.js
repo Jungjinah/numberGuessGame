befcfb502557c6e41e81ea7d92b18bb081ce9d66
Jungjinah <abo8637@naver.com>
2024-02-12 04:12:26 +0900

    thirdUpdate

--- main.js
+++ main.js
@@ -45,6 +45,13 @@ function enterKey(e) {
     }
 }
 
+//숫자 길이 두자리 수 제한
+function numberMaxLength(e) {
+    if(e.value.length > e.maxLength) {
+        e.value = e.value.slice(0, e.maxLength);
+    }
+}
+
 pickRandomNum();
 
 
